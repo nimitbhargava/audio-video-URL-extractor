6642ab68a169b8c5904fef5e4bfbbe7304a7e46a
Nimit Bhargava <nimitbhargava@gmail.com>
2017-09-12 04:21:13 +0530

    Recognizing share dropdown anchor button for audio and video download links

--- searchURLs.js
+++ searchURLs.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 const puppeteer = require('puppeteer');
@@ -17,23 +16,40 @@ const cheerio = require('cheerio');
 var audioTag = '<audio controls>    <source src="horse.ogg" type="audio/ogg">    <source src="horse.mp3" type="audio/mpeg">    Your browser does not support the audio tag.</audio>';
 // HTML <video> Tag - https://www.w3schools.com/tags/tag_video.asp
 var videoTag = '<video width="320" height="240" controls>    <source src="movie.mp4" type="video/mp4">    <source src="movie.ogg" type="video/ogg">    Your browser does not support the video tag.</video>';
-var testCase1  = '<div class="sqs-audio-embed" data-url="https://static1.squarespace.com/static/50dc8cece4b03955129656eb/t/587bb6e5db29d69a1a276170/1484502978069/701_0231+%28online-audio-converter.com%29.mp3/original/701_0231+%28online-audio-converter.com%29.mp3">'
+var testCase1 = '<div class="sqs-audio-embed" data-url="https://static1.squarespace.com/static/50dc8cece4b03955129656eb/t/587bb6e5db29d69a1a276170/1484502978069/701_0231+%28online-audio-converter.com%29.mp3/original/701_0231+%28online-audio-converter.com%29.mp3">'
 var testCase2 = '<div class="sqs-audio-embed" data-url="https://static1.squarespace.com/static/544b30c0e4b0023e70482546/t/58996bd944024399dae27e39/1486450657175/AF+Ep66.mp3/original/AF+Ep66.mp3">';
-const $ = cheerio.load(testCase2);
+var testCase3 = '<div class="dropdown share-dropdown" id="downloadsButton"><button class="share-drop dropdown-toggle" data-toggle="dropdown"><i class="fa fa-download"></i><span class="hidden-xs">download</span></button><div class="dropdown-menu" role="menu" aria-labelledby="dLabel"><a class="btn" href="http://cdnapi.kaltura.com/p/537811/sp/53781100/playManifest/entryId/1_zaatpsw0/flavorId/1_vs6gl6x6/protocol/http/format/url/a.mp4" download="john-cleese-on-creativity-group-dynamics-and-celebrity-lq.mp4">SD video</a><a class="btn" href="http://cdnapi.kaltura.com/p/537811/sp/53781100/playManifest/entryId/1_zaatpsw0/flavorId/1_9ymnkm7b/protocol/http/format/url/a.mp3" download="john-cleese-on-creativity-group-dynamics-and-celebrity.mp3">audio</a></div></div>';
+const $ = cheerio.load(testCase3);
 
 var audioSource = [], videoSource = [];
-$('audio source, video source, .sqs-audio-embed').each(function () {
+$('audio source, video source, .sqs-audio-embed, .share-dropdown > .dropdown-menu > .btn').each(function () {
     var source = $(this).attr('src');
+
     var isAudioSource = $(this).is('audio source');
-    if (isAudioSource){
+    var isSQSAudioEmbed = $(this).is('.sqs-audio-embed');
+    var isShareDropDownAnchorButton = $(this).is('.share-dropdown > .dropdown-menu > .btn');
+    var href = $(this).attr('href');
+    var hrefIsAudio = href.indexOf("mp3") !== -1;
+
+
+    if (isAudioSource) {
         audioSource.push(source);
     }
     else {
-        if ($(this).is('.sqs-audio-embed')) {
+        if (isSQSAudioEmbed) {
             audioSource.push($(this).data('url'));
         }
         else {
-            videoSource.push(source);
+            if (isShareDropDownAnchorButton) {
+                if (hrefIsAudio) {
+                    audioSource.push(href);
+                }
+                else {
+                    videoSource.push(href);
+                }
+            } else {
+                videoSource.push(source);
+            }
         }
     }
 });
