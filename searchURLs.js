f13b7e87109b6d90cd00b3828e3512f4eb43848e
Nimit Bhargava <nimitbhargava@gmail.com>
2017-09-12 03:30:57 +0530

    Combining audio and video source over one loop using multiple selector Pushing audio/video source links to their respective arrays

--- searchURLs.js
+++ searchURLs.js
@@ -15,11 +15,12 @@ const cheerio = require('cheerio');
 var testHTML = '<audio controls>    <source src="horse.ogg" type="audio/ogg">    <source src="horse.mp3" type="audio/mpeg">    Your browser does not support the audio tag.</audio><video width="320" height="240" controls>    <source src="movie.mp4" type="video/mp4">    <source src="movie.ogg" type="video/ogg">    Your browser does not support the video tag.</video>';
 const $ = cheerio.load(testHTML);
 
-$('audio source').each(function () {
-    console.log($(this).attr('src'));
-});
-$('video source').each(function () {
-    console.log($(this).attr('src'));
+var audioSource = [], videoSource = [];
+$('audio source, video source').each(function () {
+    var source = $(this).attr('src');
+    var isAudioSource = $(this).is('audio source');
+    if (isAudioSource) audioSource.push(source);
+    else videoSource.push(source);
 });
 
 browser.close();
