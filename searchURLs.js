88c5d654f7713d0c8c27eb3781b4aab5cce2246f
Nimit Bhargava <nimitbhargava@gmail.com>
2017-09-12 03:55:09 +0530

    Recognizing sqs-audio-embed class as for audio links

--- searchURLs.js
+++ searchURLs.js
@@ -7,7 +7,7 @@ const puppeteer = require('puppeteer');
 
     const browser = await puppeteer.launch();
 const page = await browser.newPage();
-await page.goto('https://www.w3schools.com/tags/tag_video.asp');
+// await page.goto('https://fireandfragrance.com/podcasts/2017/1/15/andy-byrd-boundless-love');
 
 const DOMstring = await page.content();
 
@@ -17,16 +17,25 @@ const cheerio = require('cheerio');
 var audioTag = '<audio controls>    <source src="horse.ogg" type="audio/ogg">    <source src="horse.mp3" type="audio/mpeg">    Your browser does not support the audio tag.</audio>';
 // HTML <video> Tag - https://www.w3schools.com/tags/tag_video.asp
 var videoTag = '<video width="320" height="240" controls>    <source src="movie.mp4" type="video/mp4">    <source src="movie.ogg" type="video/ogg">    Your browser does not support the video tag.</video>';
-var testHTML = audioTag + videoTag;
-
-const $ = cheerio.load(testHTML);
+var testCase1  = '<div class="sqs-audio-embed" data-url="https://static1.squarespace.com/static/50dc8cece4b03955129656eb/t/587bb6e5db29d69a1a276170/1484502978069/701_0231+%28online-audio-converter.com%29.mp3/original/701_0231+%28online-audio-converter.com%29.mp3">'
+var testCase2 = '<div class="sqs-audio-embed" data-url="https://static1.squarespace.com/static/544b30c0e4b0023e70482546/t/58996bd944024399dae27e39/1486450657175/AF+Ep66.mp3/original/AF+Ep66.mp3">';
+const $ = cheerio.load(testCase2);
 
 var audioSource = [], videoSource = [];
-$('audio source, video source').each(function () {
+$('audio source, video source, .sqs-audio-embed').each(function () {
     var source = $(this).attr('src');
     var isAudioSource = $(this).is('audio source');
-    if (isAudioSource) audioSource.push(source);
-    else videoSource.push(source);
+    if (isAudioSource){
+        audioSource.push(source);
+    }
+    else {
+        if ($(this).is('.sqs-audio-embed')) {
+            audioSource.push($(this).data('url'));
+        }
+        else {
+            videoSource.push(source);
+        }
+    }
 });
 
 if (audioSource.length) {
