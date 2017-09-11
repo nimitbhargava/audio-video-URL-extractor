66e0abcb3545ba27c45a38fb8dac979ade2f8493
Nimit Bhargava <nimitbhargava@gmail.com>
2017-09-12 03:21:29 +0530

    Displaying audio video link source in console

--- searchURLs.js
+++ searchURLs.js
@@ -15,6 +15,13 @@ const cheerio = require('cheerio');
 var testHTML = '<audio controls>    <source src="horse.ogg" type="audio/ogg">    <source src="horse.mp3" type="audio/mpeg">    Your browser does not support the audio tag.</audio><video width="320" height="240" controls>    <source src="movie.mp4" type="video/mp4">    <source src="movie.ogg" type="video/ogg">    Your browser does not support the video tag.</video>';
 const $ = cheerio.load(testHTML);
 
+$('audio source').each(function () {
+    console.log($(this).attr('src'));
+});
+$('video source').each(function () {
+    console.log($(this).attr('src'));
+});
+
 browser.close();
 
 })();
